aaee71e28b15c891b75d45a6e8ec44750f61855c
SamuelRicardoDS <samuelricardods@gmail.com>
2023-09-12 14:18:32 -0300

    fix(): full hd circle

--- client/src/components/VersceCircle.tsx
+++ client/src/components/VersceCircle.tsx
@@ -1,9 +1,9 @@
 export const VersceCircle = () => {
     return(
         <div className=" flex w-screen h-auto justify-center items-center">
-            <div className="text-6xl absolute">VERSCE</div>
+            <div className="text-6xl absolute mr-96  mb-20">VERSCE</div>
             <div className="bg-transparent w-80 h-80 border-8 rounded-full border-white" />
-            <div className="text-6xl absolute ">MARKETING</div>
+            <div className="text-6xl absolute ml-96 mt-20">MARKETING</div>
         </div>
     )
 }
